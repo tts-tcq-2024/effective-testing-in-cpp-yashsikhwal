df4919cb492a7119ee12e7ddc5547acbde1c5a0d
Yash Sikhwal <56091569+yashsikhwal@users.noreply.github.com>
2024-10-11 11:22:31 +0530

    Add test case to tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -17,6 +17,8 @@ int main() {
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
+    assert(size(38) == 'S');
+    assert(size(42) == 'L');
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
