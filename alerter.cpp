c6f86e51d0e91aa6107a45fdb72bd5823b07d6e6
Yash Sikhwal <56091569+yashsikhwal@users.noreply.github.com>
2024-10-11 12:01:53 +0530

    Add test case to alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -26,6 +26,7 @@ void alertInCelcius(float farenheit) {
 int main() {
     alertInCelcius(400.5);
     alertInCelcius(303.6);
+    assert(alertFailureCount == 2)
     std::cout << alertFailureCount << " alerts failed.\n";
     std::cout << "All is well (maybe!)\n";
     return 0;
