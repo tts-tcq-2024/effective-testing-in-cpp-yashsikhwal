481605294709ceef6f4c46dd12316f257d078fdb
Yash Sikhwal <56091569+yashsikhwal@users.noreply.github.com>
2024-10-11 12:00:56 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -8,7 +8,7 @@ int networkAlertStub(float celcius) {
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
-    return 200;
+    return 500;
 }
 
 void alertInCelcius(float farenheit) {
